e2be94a2a981045a2c33741cedbe67d00a23b8f3
angelopoole <8thgenwolf@gmail.com>
2016-04-06 22:25:35 +0000

    leaving bootstrap for now focusing on css

--- javascript.js
+++ javascript.js
@@ -11,3 +11,19 @@ webrtc.on('readyToCall', function () {
     // you can name it anything
     webrtc.joinRoom('your awesome room name');
 });
+
+
+(function(){
+var box = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = 'chat';
+PUBNUB.subscribe({
+    channel  : channel,
+    callback : function(text) { box.innerHTML = (''+text).replace( /[<>]/g, '' ) + '<br>' + box.innerHTML }
+});
+PUBNUB.bind( 'keyup', input, function(e) {
+    (e.keyCode || e.charCode) === 13 && PUBNUB.publish({
+        channel : channel, message : input.value, x : (input.value='')
+    })
+} )
+})()
+PUBNUB.publish({ channel : 'chat', message : "hello!" })
+PUBNUB.subscribe({ channel : 'chat', message : fun })
